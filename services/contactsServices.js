3ff3f5bf32c5adc3b38e85a1f3502fe7ef7ed2bb
ViktoriaSharova <131592560+ViktoriaSharova@users.noreply.github.com>
2024-04-11 23:02:48 +0200

    fixed

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,13 +1,13 @@
 import Contact from "../models/contactModel.js";
 
-export const getAllContacts = () => Contact.find();
+export const getAllContacts = (owner, options) => Contact.find({ owner }).skip((options.page - 1) * options.limit).limit(options.limit);
 
-export const getOneContact = (id) => Contact.findById(id);
+export const getOneContact = (id, owner) => Contact.findOne({ _id: id, owner });
 
-export const deleteContact = (id) => Contact.findByIdAndDelete(id);
+export const deleteContact = (id, owner) => Contact.findOneAndDelete({ _id: id, owner });
 
 export const createContact = (contactData) => Contact.create(contactData);
 
-export const updateContact = (id, data) => Contact.findByIdAndUpdate(id, data, { new: true });
+export const updateContact = (id, body, owner, options) => Contact.findOneAndUpdate({ _id: id, owner }, body, options);
 
-export const updateStatusContact = (contactId, data, options) => Contact.findByIdAndUpdate(contactId, data, options);
+export const updateStatusContact = (id, body, owner, options) => Contact.findOneAndUpdate({ _id: id, owner: owner }, body, options);
